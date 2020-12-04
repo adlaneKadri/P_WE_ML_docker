f62a2c97866f2cbce1fada85c71699e53b1446cd
adlankadri <akadri@innoscape.com>
2020-12-04 10:46:50 +0100

    1st version backend

---
+++ P_WE_ML/backend/preprocessing/processing.py
@@ -0,0 +1,90 @@
+import sys
+sys.path.insert(1, "..")
+import os
+import nltk
+import warnings
+warnings.simplefilter(action='ignore')
+import pandas as pd
+from all_functions.config import remove_empty_words_1, tokenize_data, remove_stop_words, stemming, lemmatization, remove_garbage, create_tf_idf, tf_idf_transform_data
+import pickle
+
+nltk.download('wordnet')
+nltk.download('stopwords')
+PATH = os.path.dirname(os.getcwd())
+
+def load_data(positive_train_data_path, negative_train_data_path):
+	#print(positive_train_data_path)
+	data = []
+	for file in os.listdir(positive_train_data_path):
+		with open(os.path.join(positive_train_data_path + file), "r") as f:
+			line = f.readlines()[0]
+			data.append({"reviews": line, "labels":1})
+	
+	for file in os.listdir(negative_train_data_path):
+		with open(os.path.join(negative_train_data_path + file), "r") as f:
+			line = f.readlines()[0]
+			data.append({"reviews": line, "labels":0})
+
+	dataset = pd.DataFrame(data)
+	#dataset = data
+	#print(pos.head(5))
+	return dataset
+
+
+def training_data():
+
+	pos_train_data_path = os.path.join(PATH, "dataset", "pos", "")
+	neg_train_data_path = os.path.join(PATH, "dataset", "neg", "")
+	dataset = load_data(pos_train_data_path, neg_train_data_path)
+
+	dataset = remove_empty_words_1(dataset)
+	dataset = tokenize_data(dataset)
+	dataset = remove_stop_words(dataset)
+	dataset = stemming(dataset)
+	dataset = lemmatization(dataset)
+	dataset = remove_garbage(dataset)
+	return dataset
+
+def testing_data():	
+	pos_train_data_path = os.path.join(PATH, "dataset", "pos", "")
+	neg_train_data_path = os.path.join(PATH, "dataset", "neg", "")
+	dataset = load_data(pos_train_data_path, neg_train_data_path)
+	dataset = remove_empty_words_1(dataset)
+	dataset = tokenize_data(dataset)
+	dataset = remove_stop_words(dataset)
+	dataset = stemming(dataset)
+	dataset = lemmatization(dataset)
+	dataset = remove_garbage(dataset)
+	return dataset
+
+def main():
+	print("------------------ data loading ---------------------")
+	train_data = training_data()
+	test_data = testing_data()
+	
+	pos_train_data_xls = os.path.join(PATH, "dataset","train_data.xlsx")
+	pos_test_data_xls = os.path.join(PATH, "dataset","test_data.xlsx")
+	
+	train_data.to_excel(pos_train_data_xls, index = False, engine='xlsxwriter')
+	test_data.to_excel(pos_test_data_xls, index = False, engine='xlsxwriter')
+	
+	train_data.to_csv ("../dataset/train_data.csv", index = False)
+	test_data.to_csv ("../dataset/test_data.csv", index = False)
+
+
+	# print("------------------------ creating tfidf ------------------------ ")
+	# tfidf = create_tf_idf(train_data, test_data)  #Fitting the vecorizer
+	
+	# print("------------------------ transforme tfidf ------------------------ ")
+	# train_features = tf_idf_transform_data(tfidf, train_data)  #transforming 
+	# test_features = tf_idf_transform_data(tfidf, test_data)    #Train and Test
+	# train_labels = train_data["labels"]  #Taking lables in separate
+	# test_labels = test_data["labels"]    #variables
+	
+	# print("------------------------  saving tfidf ------------------------ ")
+	# pickle.dump(tfidf, open("../models/tfidf.pickle", "wb"))
+	# pickle.dump(train_features, open("../../models/train_features.pickle", "wb"))
+	# pickle.dump(test_features, open("../../models/test_features.pickle", "wb"))
+
+
+main()
---
+++ P_WE_ML/backend/word_embeding/bow/bag_of_word.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.insert(1, "../..")
+import os
+import warnings
+warnings.simplefilter(action='ignore')
+import pandas as pd
+from all_functions.config import remove_empty_words_1, tokenize_data, remove_stop_words, stemming, lemmatization, remove_garbage, create_bow, bow_transform_data
+import pickle
+
+PATH = os.getcwd()
+
+
+def load_data():
+	train_data = pd.read_excel('../../dataset/train_data.xlsx') 
+	test_data = pd.read_excel('../../dataset/test_data.xlsx') 
+	train_labels = train_data["labels"]  #Taking lables in separate
+	test_labels = test_data["labels"]    #variables
+	return train_data, test_data, train_labels, test_labels
+
+def main():
+	print("------------------------  load data  --------------------------- ")
+	train_data, test_data, train_labels, test_labels = load_data()
+
+
+	print("------------------------ creating BOW ------------------------ ")
+	bow = create_bow(train_data, test_data)  #Fitting the vecorizer
+	
+	print("------------------------ transforme BOW ------------------------ ")
+	train_features = bow_transform_data(bow, train_data)  #transforming 
+	test_features = bow_transform_data(bow, test_data)    #Train and Test
+	train_labels = train_data["labels"]  #Taking lables in separate
+	test_labels = test_data["labels"]    #variables
+	
+	print("------------------------  saving BOG ------------------------ ")
+	pickle.dump(bow, open("../../../models/bow.pickle", "wb"))
+	pickle.dump(train_features, open("../../../models/train_features_bow.pickle", "wb"))
+	pickle.dump(test_features, open("../../../models/test_features_bow.pickle", "wb"))
+
+
+main()
