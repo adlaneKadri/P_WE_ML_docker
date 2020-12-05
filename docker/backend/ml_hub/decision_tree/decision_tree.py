bc4be683233d6b99171f3163bc459d4056057d9c
adlankadri <akadri@innoscape.com>
2020-12-05 23:25:05 +0100

    dockerization of P_WE_ML project

---
+++ docker/backend/ml_hub/decision_tree/decision_tree.py
@@ -0,0 +1,44 @@
+
+import os
+import warnings
+warnings.simplefilter(action='ignore')
+import pandas as pd
+import pickle
+from sklearn.tree import DecisionTreeClassifier
+
+# nltk.download('wordnet')
+# nltk.download('stopwords')
+PATH = os.getcwd()
+
+
+
+def load_data():
+	train_data = pd.read_excel('dataset/train_data.xlsx') 
+	test_data = pd.read_excel('dataset/test_data.xlsx') 
+	train_labels = train_data["labels"]  #Taking lables in separate
+	test_labels = test_data["labels"]    #variables
+	return train_data, test_data, train_labels, test_labels
+
+def load_encoding_model():
+	tfidf = pickle.load(open("models/tfidf.pickle", "rb"))
+	train_features = pickle.load(open("models/train_features.pickle", "rb"))
+	test_features = pickle.load(open("models/test_features.pickle", "rb"))	
+	return tfidf, train_features, test_features
+
+def main():
+	print("------------------     load data     --------------------- ")
+	train_data, test_data, train_labels, test_labels = load_data()
+
+	print("------------------ load encoding models ------------------ ")
+	tfidf, train_features, test_features = load_encoding_model()
+	
+	print("------------------      DT model      -------------------- ")
+	clf = DecisionTreeClassifier(random_state=0)
+	clf.fit(train_features, train_labels)
+	score = clf.score(test_features, test_labels)
+	print("score is : {}%".format(score*100))
+	pickle.dump(clf, open("models/LogisticRegression.pickle", "wb"))
+
+
+
+main()
