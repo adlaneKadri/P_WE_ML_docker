ab2fafac5db6bbe1de3f9d54f946894e8396369a
KADRI Adlane <adlan68@live.fr>
2020-12-06 14:28:54 +0100

    Update display_flask.py

--- docker/frontend/display_flask.py
+++ docker/frontend/display_flask.py
@@ -68,4 +68,4 @@ def prediction(line):
 
 port = int(os.environ.get("PORT", 5000))
 if __name__ == '__main__' : 
-	app.run(debug=True, host='127.0.0.1',port=port)
+	app.run(debug=True, host='0.0.0.0',port=port)
